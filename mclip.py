4ec72b2bbe912c8c3916ece6cc39fad9a1518dd2
sarthoksarik <kirasnamdas@gmail.com>
2020-01-21 17:21:24 +0600

    2nd

--- mclip.py
+++ mclip.py
@@ -1,3 +1,15 @@
 #! python3
-# clipboard program for pasting phrases
+# clipboard program for pasting phrase
 
+import sys
+
+TEXT = {'biwta': 'Bangladesh Inland Water Transport Authority',
+        'sss': 'Sarik Sadman',
+        'adsss': '309//1//1, East Nakhalpara, Tejgaon, Dhaka-1215',
+        'nidsss': '4619767058'}
+
+if len(sys.argv) < 2:
+    print('Usage: python mclip.py [keyphrase] - copy phrase text')
+    sys.exit()
+# the first command line argument is the keyphrase
+keyphrase = sys.argv[1]
