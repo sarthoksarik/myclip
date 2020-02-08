aa08a1bc100cdbfbbf5e30f57ca2d817083d2c99
sarthoksarik <kirasnamdas@gmail.com>
2020-02-08 12:03:12 +0600

    final edit

--- mclip.py
+++ mclip.py
@@ -11,5 +11,11 @@ TEXT = {'biwta': 'Bangladesh Inland Water Transport Authority',
 if len(sys.argv) < 2:
     print('Usage: python mclip.py [keyphrase] - copy phrase text')
     sys.exit()
+
+if keyphrase in TEXT:
+    pyperclip.copy(TEXT[keyphrase])
+    print('Text for ' + keyphrase + 'copied to clipboard')
+else:
+    print('There is no text for ' + keyphrase)
 # the first command line argument is the keyphrase
 keyphrase = sys.argv[1]
